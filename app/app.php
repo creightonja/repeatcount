6e48704f12dabd7c33d8f812c8c226e31fe8159c
creightonja <creightonja@gmail.com>
2015-08-14 11:40:01 -0700

    finish the app.

---
+++ app/app.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    //loads symfony debug component
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    //Initializes Silex
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    //Setting twig path to views folder
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        "twig.path" => __DIR__."/../views"
+    ));
+
+    //Setting default home page to submit.html.twig
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("submit.html.twig", array('result' => " "));
+    });
+
+    $app->get("/result", function() use ($app) {
+        $repeat_count_list = new RepeatCounter;
+        $repeat_count = $repeat_count_list->countRepeats($_GET['input_word'], $_GET['input_check_list']);
+        return $app["twig"]->render("submit.html.twig", array('result' => $repeat_count));
+    });
+
+    return $app;
+
+?>
--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,16 +5,16 @@
         function countRepeats($input_word, $input_check_words) {
             $lower_case_input = strtolower($input_word);
             $lower_case_list = strtolower($input_check_words);
-            $matchcounter = 0;
+            $match_counter = 0;
             $exploded_list = explode(" ", $lower_case_list);
 
             foreach ($exploded_list as $word) {
                 if ($word == $lower_case_input) {
-                    $matchcounter++;
+                    $match_counter++;
                 }
             }
 
-            return $matchcounter;
+            return $match_counter;
         }
 
 
