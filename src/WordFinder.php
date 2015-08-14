78499e810f691b5f980a51f3dfb0baa892023908
creightonja <creightonja@gmail.com>
2015-08-14 10:13:57 -0700

    add first test and initialize files.

---
+++ src/WordFinder.php
@@ -0,0 +1,14 @@
+<?php
+
+    class WordFinder {
+
+        function makeWord($input_word, $input_check_word) {
+            $output = $input_check_word;
+
+            return $output;
+        }
+
+
+    } //End Class
+
+?>
